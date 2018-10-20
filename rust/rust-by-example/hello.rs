ce597bb3bcd48156a9645304085894b27314b630
SeHun Shin <s.shin@marathoner.kr>
2018-10-20 17:34:07 +0900

    Update hello.rs
    comments

--- rust/rust-by-example/hello.rs
+++ rust/rust-by-example/hello.rs
@@ -1,4 +1,18 @@
 fn main() {
+    // Line comments
+    /*
+    Block Comments
+    */
+    
+    // println!("Rust or Go?")
+    /*
+    public static void main(String[] args) {
+        System.out.print.ln("Hello, World!");
+    }
+    */
     println!("Hello World!");
     println!("I'm Rustacean!\nAre you?");
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
 }
