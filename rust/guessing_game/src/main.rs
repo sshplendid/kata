74d8a015e2c45a8fec250e2e7b9f2a4cd982226c
SeHun Shin <s.shin@marathoner.kr>
2018-10-24 16:17:51 +0900

    refactor: main.rs
    함수 추출

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -9,7 +9,11 @@ fn main() {
     
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
+    guess_number(&secret_number);
+}
+
+fn guess_number(secret_number: &u32) {
     loop {
         println!("Please input your guess.");
 
@@ -18,23 +22,43 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                continue;
-            }
-        };
+        let guess: u32 = parse_guess_to_number(&guess);
+        
+        if guess.eq(&0) {
+            continue;
+        }
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater  => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+        if compare_number(&guess, &secret_number) {
+            break;
+        }
+    }
+}
+
+fn compare_number(actual: &u32, expected: &u32) -> bool {
+    match actual.cmp(&expected) {
+        Ordering::Less => {
+            println!("Too small!");
+            false
+        },
+        Ordering::Greater => {
+            println!("Too big!");
+            false
+        },
+        Ordering::Equal => {
+            println!("You win!");
+            true
+        }
+    }
+}
+
+fn parse_guess_to_number(guess_str: &str) -> u32 {
+    match guess_str.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please type a number!");
+            0
         }
     }
 }
