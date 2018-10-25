3307fa2e802e16eab0d1e08cc4d2884fd0b087fd
SeHun Shin <s.shin@marathoner.kr>
2018-10-26 00:01:52 +0900

    example: shadowing - 변수 유형을 변경할 수 있다.

--- rust/rust_lang_book/shadow/src/main.rs
+++ rust/rust_lang_book/shadow/src/main.rs
@@ -6,4 +6,11 @@ fn main() {
     let x = x * 2; // 6을 가진 x는 shadow되었다.
 
     println!("The value of x is {}", x);
+
+
+    let spaces = "    ";
+    println!("spaces >{}<", spaces);
+
+    let spaces = spaces.len(); // 값의 유형을 변경할 수 있으면서도 동일한 이름을 사용할 수 있다.
+    println!("It has {} spaces.", spaces);
 }
