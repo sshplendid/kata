adeac94ba7f059b53d823ed34e5c8f455e9a1c0b
SeHun Shin <s.shin@marathoner.kr>
2018-10-27 20:08:41 +0900

    Update main.rs
    불필요한 레퍼런스 제거

--- rust/rust_lang_book/guessing_game/src/main.rs
+++ rust/rust_lang_book/guessing_game/src/main.rs
@@ -6,14 +6,14 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     // println!("The secret number is: {}", secret_number);
-    guess_number(&secret_number);
+    guess_number(secret_number);
 }
 
-fn guess_number(secret_number: &u32) {
+fn guess_number(secret_number: u32) {
     loop {
         println!("Please input your guess.");
 
@@ -23,20 +23,20 @@ fn guess_number(secret_number: &u32) {
             .expect("Failed to read line");
 
         let guess: u32 = parse_guess_to_number(&guess);
-        
+
         if guess.eq(&0) {
             continue;
         }
 
         println!("You guessed: {}", guess);
 
-        if compare_number(&guess, &secret_number) {
+        if compare_number(guess, secret_number) {
             break;
         }
     }
 }
 
-fn compare_number(actual: &u32, expected: &u32) -> bool {
+fn compare_number(actual: u32, expected: u32) -> bool {
     match actual.cmp(&expected) {
         Ordering::Less => {
             println!("Too small!");
