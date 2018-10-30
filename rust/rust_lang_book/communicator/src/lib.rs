6195edb8ff1a81941d159fcd7cd2890972597e44
SeHun Shin <s.shin@marathoner.kr>
2018-10-31 00:16:54 +0900

    📝 모듈: import name & super

--- rust/rust_lang_book/communicator/src/lib.rs
+++ rust/rust_lang_book/communicator/src/lib.rs
@@ -14,6 +14,17 @@ pub mod network;
 mod tests {
     #[test]
     fn it_works() {
+/*
+ * communicator // (1)번: 루트 라이브러리로 올라가서 하위 라이브러리 접근, 라이브러리의 규모가 큰
+ * 경우 복잡해질 수 있다.
+ * ├─client    **여길 찾아가야 함
+ * ├─test      // (2)번: 현재 위치에서 부모 모듈에 접근한 뒤 client 라이브러리에 접근
+ * └─network
+ *   ├─server
+ *   └─client
+ */
+        ::client::connect();      // (1)번
+        super::client::connect(); // (2)번
         assert_eq!(2 + 2, 4);
     }
 }
