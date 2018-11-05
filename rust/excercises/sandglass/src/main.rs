3ab81aa14ee1c3dea6f5904d02cf2a9018eebf3a
SeHun Shin <s.shin@marathoner.kr>
2018-11-05 23:07:51 +0900

    🛠  절대값 함수 refactoring

--- rust/excercises/sandglass/src/main.rs
+++ rust/excercises/sandglass/src/main.rs
@@ -30,25 +30,27 @@ fn sandglass(i: i32) {
     let half = i / 2;
     for n in 0..i {
         let mut k = half - n;
+        k = abs(k);
+        // spaces
+        for a in 0..(half - k) {
+            print!(" ");
+        }
+        let mut k = i - n * 2;
         if (k < 0) {
-            k = k * (-1);
+            k = abs(k - 2);
         }
-        {
-            // spaces
-            for a in 0..(half - k) {
-                print!(" ");
-            }
-        }
-        {
-            let mut k = i - n * 2;
-            if (k < 0) {
-                k = (k-2) * (-1);
-            }
-            // stars
-            for b in 0..k {
-                print!("*");
-            }
+        // stars
+        for b in 0..k {
+            print!("*");
         }
         print!("\n");
     }
+}
+
+fn abs(number: i32) -> i32 {
+    let mut result = number;
+    if (result < 0) {
+        result = result * (-1);
+    }
+    result
 }
