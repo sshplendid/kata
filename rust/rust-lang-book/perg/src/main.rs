10a36109cf7126308f640077d7f2893ec1583d54
SeHun Shin <s.shin@marathoner.kr>
2018-11-17 02:46:00 +0900

    refactor: main.rs

--- rust/rust-lang-book/perg/src/main.rs
+++ rust/rust-lang-book/perg/src/main.rs
@@ -5,15 +5,27 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config  = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut file = File::open(filename).expect("file not found");
+    let mut file = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("something went wrong reading the file");
     println!("With text:\n{}", contents);
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+}
+
