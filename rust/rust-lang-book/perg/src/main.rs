77a75da41af6a89767d5b2686f81e8748f1988c3
SeHun Shin <s.shin@marathoner.kr>
2018-11-17 03:28:19 +0900

    refactor: lib.rs

--- rust/rust-lang-book/perg/src/main.rs
+++ rust/rust-lang-book/perg/src/main.rs
@@ -1,31 +1,24 @@
+extern crate perg;
+
+use std::process;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use perg::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config  = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut file = File::open(config.filename).expect("file not found");
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    if let Err(e) = perg::run(config) {
+        println!("Application Error: {}", e);
+
+        process::exit(1);
+    };
 }
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
-
