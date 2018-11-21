8218a392f1caa2aa70d763b8b48bd621ba86218f
SeHun Shin <s.shin@marathoner.kr>
2018-11-21 16:51:57 +0900

    feat: error print

--- rust/rust-lang-book/perg/src/main.rs
+++ rust/rust-lang-book/perg/src/main.rs
@@ -1,19 +1,19 @@
 extern crate perg;
 
-use std::process;
 use std::env;
+use std::process;
 use perg::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = perg::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
 
         process::exit(1);
     };
