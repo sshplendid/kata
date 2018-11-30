eb62588a6b154bcf317f4b184658e1c3284d1eea
SeHun Shin <s.shin@marathoner.kr>
2018-11-30 19:37:37 +0900

    ch10-7: x와 y 필드의 타입이 다를 경우

--- rust/rust-lang-book/point/src/main.rs
+++ rust/rust-lang-book/point/src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.1, y: 4.1 };
-
-    println!("integer point {:?}", integer);
-    println!("float point {:?}", float);
+    test_will_work();
+    test_wont_work();
 }
 
 #[derive(Debug)]
@@ -13,3 +10,16 @@ struct Point<T> {
 }
 
 
+fn test_will_work() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.1, y: 4.1 };
+
+    println!("integer point {:?}", integer);
+    println!("float point {:?}", float);
+}
+
+fn test_wont_work() {
+    let wont_work = Point { x:1, y: 2.1 };
+
+    println!("{:?}", wont_work);
+}
