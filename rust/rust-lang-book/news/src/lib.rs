3b9283a580b0f55b4857d3f5ad7e395d35207259
SeHun Shin <s.shin@marathoner.kr>
2018-12-01 19:29:16 +0900

    ch10-12: NewsArticle과 Tweet 타입 상에서의 Summarizable 트레잇 구현

---
+++ rust/rust-lang-book/news/src/lib.rs
@@ -0,0 +1,52 @@
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String, 
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    use Tweet;
+    use Summarizable;
+    #[test]
+    fn tweet_test() {
+        let tweet = Tweet {
+            username: String::from("via_ssh"),
+            content: String::from("I'm learning Rust Language."),
+            reply: false,
+            retweet: false,
+        };
+
+        println!("1 new tweet: {}", tweet.summary());
+    }
+}
