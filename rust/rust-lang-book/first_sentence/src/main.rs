8929231f7ecf408a59b249d3d8e6852059f18bf3
SeHun Shin <s.shin@marathoner.kr>
2018-12-05 09:59:52 +0900

    ch10: 메소드 정의 내에서의 라이프타임 명시

--- rust/rust-lang-book/first_sentence/src/main.rs
+++ rust/rust-lang-book/first_sentence/src/main.rs
@@ -3,6 +3,17 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 { // 반환 값은 self와 같은 lifetime을 가진다.
+        3
+    }
+
+    fn announce_and_return_part(&self, announcemenet: &str) -> &str {
+        println!("Attention please: {}", announcemenet);
+        self.part
+    }
+}
+
 fn main() {
     let novel = String::from("Call me Ishmael. Some years ago...");
 
@@ -13,4 +24,7 @@ fn main() {
     let i = ImportantExcerpt { part: first_sentence };
 
     println!("first line is '{}.'", i.part);
+    
+    let part = i.announce_and_return_part("에헴");
+    println!("part -> {}", part);
 }
