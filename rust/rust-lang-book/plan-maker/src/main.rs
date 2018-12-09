90c893e8b85dd89da5dd4eb801fee712b9d64a6f
SeHun Shin <s.shin@marathoner.kr>
2018-12-09 17:49:42 +0900

    ch13-3: 입력값과 함수 호출에 근거해서 운동 계획을 출력하는 비즈니스 로직

--- rust/rust-lang-book/plan-maker/src/main.rs
+++ rust/rust-lang-book/plan-maker/src/main.rs
@@ -1,9 +1,16 @@
 use std::thread;
 use std::time::Duration;
+use std::env;
 
 fn main() {
-    simulated_expensive_calculation(2);
+    let args:Vec<String> = env::args().collect();
 
+    let simulated_user_specified_value: u32 = args[1].parse().unwrap();
+    let simulated_random_number: u32 = args[2].parse().unwrap();
+
+    println!("{} {}", simulated_user_specified_value, simulated_random_number);
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
@@ -11,4 +18,17 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     thread::sleep(Duration::from_secs(2));
     println!("finished... intensity: {}", intensity);
     intensity
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!("Today, do {} pushups!", simulated_expensive_calculation(intensity));
+        println!("Next, do {} situps!", simulated_expensive_calculation(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", simulated_expensive_calculation(intensity));
+        }
+    }
 }
