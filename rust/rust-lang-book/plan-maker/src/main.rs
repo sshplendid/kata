e658271e64af1d596cc346b8e415e639ba4c0ee4
SeHun Shin <s.shin@marathoner.kr>
2018-12-10 16:42:38 +0900

    ch13-11: 캐싱로직 추상화를 위해 Cacher 사용

--- rust/rust-lang-book/plan-maker/src/main.rs
+++ rust/rust-lang-book/plan-maker/src/main.rs
@@ -16,30 +16,33 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
+/*
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     println!("finished... intensity: {}", intensity);
     intensity
 }
+*/
 
 fn generate_workout(intensity: u32, random_number: u32) {
     //let expensive_result = simulated_expensive_calculation(intensity);
 
-    let expensive_closure = |num: u32| -> u32 {
+    let mut expensive_result = Cacher::new(
+    |num: u32| -> u32 {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity));
-        println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_closure(intensity));
+            println!("Today, run for {} minutes!", expensive_result.value(intensity));
         }
     }
 }
