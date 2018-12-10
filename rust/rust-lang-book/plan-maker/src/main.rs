fa04aa0708aa2b8d84d45b1d787e17c2392ca4df
SeHun Shin <s.shin@marathoner.kr>
2018-12-10 16:59:48 +0900

    ch13: move 클로저 예제

--- rust/rust-lang-book/plan-maker/src/main.rs
+++ rust/rust-lang-book/plan-maker/src/main.rs
@@ -92,17 +92,30 @@ fn call_with_different_value() {
     let v1 = c.value(1);
     let v1 = c.value(2);
 
-    assert_eq!(v1, 2);
+    assert_ne!(v1, 2);
 }
 
 #[test]
 fn lexical_scope_test() {
     let x = 4;
 
-    //let equal_to_x = |z| z == x;
-    fn equal_to_x(z: i32) -> bool { z == x }
+    let equal_to_x = |z| z == x;
+    //fn equal_to_x(z: i32) -> bool { z == x }
 
     let y = 4;
+
+    assert!(equal_to_x(y));
+}
+
+#[test]
+fn move_test() {
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    //println!("can't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
 
     assert!(equal_to_x(y));
 }
