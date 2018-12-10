6220a6dac4961d443114212bcaa05deba22ddb4f
SeHun Shin <s.shin@marathoner.kr>
2018-12-10 11:24:58 +0900

    ch13-7: 클로저의 파라미터와 리턴 타입에 대한 타입어노테이션 추가

--- rust/rust-lang-book/plan-maker/src/main.rs
+++ rust/rust-lang-book/plan-maker/src/main.rs
@@ -24,9 +24,9 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    //let expensive_result = simulated_expensive_calculation(intensity);
 
-    let expensive_closure = |num| {
+    let expensive_closure = |num: u32| -> u32 {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
