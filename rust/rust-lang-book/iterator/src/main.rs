dd14f906334b189a6ac6547e3058a27ac7c3c804
SeHun Shin <s.shin@marathoner.kr>
2018-12-11 14:24:51 +0900

    ch13-14: 반복자 생성 & for 루프에서 반복자 사용

---
+++ rust/rust-lang-book/iterator/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    create_iterator();
+}
+
+fn create_iterator() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+}
