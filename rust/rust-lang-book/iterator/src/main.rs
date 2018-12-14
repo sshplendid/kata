fcea5ba265c2fb4314766f9e0913518dbf281d13
SeHun Shin <s.shin@marathoner.kr>
2018-12-14 15:57:11 +0900

    ch13-23: counter 반복자에 대해 트레잇의 다양한 메서드 사용하기

--- rust/rust-lang-book/iterator/src/main.rs
+++ rust/rust-lang-book/iterator/src/main.rs
@@ -127,11 +127,22 @@ mod tests {
     fn counter_iterator_next() {
         let mut counter = Counter::new();
 
-        assert_eq!(counter.next() , Some(1));
-        assert_eq!(counter.next() , Some(2));
-        assert_eq!(counter.next() , Some(3));
-        assert_eq!(counter.next() , Some(4));
-        assert_eq!(counter.next() , Some(5));
-        assert_eq!(counter.next() , None);
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iter_trait_methods() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1)) // [(1,2), (2, 3), (3, 4), (4, 5)]
+            .map(|(a, b)| a * b) // [2, 6, 12, 20]
+            .filter(|x| x % 3 == 0) // [6, 12]
+            .sum(); // 18
+
+        assert_eq!(18, sum);
     }
 }
