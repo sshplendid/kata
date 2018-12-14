a8bd6ee0b81849105b8fb38d3b488ed29f6bc5a6
SeHun Shin <s.shin@marathoner.kr>
2018-12-14 15:26:12 +0900

    ch13-12: next 메서드 구현 기능테스트

--- rust/rust-lang-book/iterator/src/main.rs
+++ rust/rust-lang-book/iterator/src/main.rs
@@ -12,6 +12,43 @@ fn create_iterator() {
     }
 }
 
+// 환경을 캡쳐하는 클로저
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+// Iterator 트레잇
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -85,14 +122,16 @@ mod tests {
             ]
         );
     }
-}
 
-#[derive(PartialEq, Debug)]
-struct Shoe {
-    size: u32,
-    style: String,
-}
+    #[test]
+    fn counter_iterator_next() {
+        let mut counter = Counter::new();
 
-fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
-    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+        assert_eq!(counter.next() , Some(1));
+        assert_eq!(counter.next() , Some(2));
+        assert_eq!(counter.next() , Some(3));
+        assert_eq!(counter.next() , Some(4));
+        assert_eq!(counter.next() , Some(5));
+        assert_eq!(counter.next() , None);
+    }
 }
