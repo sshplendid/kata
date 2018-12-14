d1b7a51a574848261f1e9f22077cb81fa9951440
SeHun Shin <s.shin@marathoner.kr>
2018-12-14 10:26:00 +0900

    ch13-16: 반복자의 모든 항목에 대한 합계 얻기

--- rust/rust-lang-book/iterator/src/main.rs
+++ rust/rust-lang-book/iterator/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
     create_iterator();
-    map_example();
 }
 
 fn create_iterator() {
@@ -13,39 +12,41 @@ fn create_iterator() {
     }
 }
 
-#[test]
-fn iterator_demonstartion() {
-    let v1 = vec![1, 2, 3];
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn iterator_demonstartion() {
+        let v1 = vec![1, 2, 3];
 
-    let mut v1_iter = v1.iter();
+        let mut v1_iter = v1.iter();
 
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
-}
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
 
-#[test]
-fn iterator_sum() {
-    let v1 = vec![1, 2, 3];
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
 
-    let v1_iter = v1.iter();
+        let v1_iter = v1.iter();
 
-    let total: i32 = v1_iter.sum();
+        let total: i32 = v1_iter.sum();
 
-    assert_eq!(total, 6);
+        assert_eq!(total, 6);
 
-    //println!("{:?}", v1_iter); // moved value
-}
+        //println!("{:?}", v1_iter); // moved value
+    }
 
+    #[test]
+    fn map_example() {
+        let v1: Vec<i32> = vec![1, 2, 3];
 
-#[test]
-fn map_example() {
-    let v1: Vec<i32> = vec![1, 2, 3];
+        let result: Vec<_> = v1.iter().map(|x| x + 1).collect();
 
-    let result: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        println!("{:?}", result);
 
-    println!("{:?}", result);
-
-    assert_eq!(result, vec![2, 3, 4]);
+        assert_eq!(result, vec![2, 3, 4]);
+    }
 }
