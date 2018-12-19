61b77fcb296ace444486d0fc78a57a4c3266799f
SeHun Shin <s.shin@marathoner.kr>
2018-12-19 05:52:44 +0000

    15-5: 알려진 크기를 갖도록 하귀 위해 Box<T>를 이용하는 List 정의

--- rust/rust-lang-book/smart-pointer/src/main.rs
+++ rust/rust-lang-book/smart-pointer/src/main.rs
@@ -1,9 +1,22 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use self::List::{Cons, Nil};
+
 fn main() {
     store_data_with_box();
-    println!("Hello, world!");
+    store_cons_list();
 }
 
 fn store_data_with_box() {
     let b = Box::new(5);
     println!("b = {}", b);
+}
+
+fn store_cons_list() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("{:?}", list);
 }
