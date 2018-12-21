10eb4f34cc600b9d1e89de2bf1f980dd93beca23
SeHun Shin <s.shin@marathoner.kr>
2018-12-21 05:40:54 +0000

    ch15-19: 참조카운트 출력

--- rust/rust-lang-book/ref-counter/src/main.rs
+++ rust/rust-lang-book/ref-counter/src/main.rs
@@ -13,11 +13,17 @@ fn main() {
 
 fn ref_counter_test() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("a -> {:?}", a);
+    println!("count after creating a = {}", Rc::strong_count(&a));
 
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("b -> {:?}", b);
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("  c -> {:?}", c);
+        println!("  count after creating a = {}", Rc::strong_count(&a));
+    }
 
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{:?}", c);
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
