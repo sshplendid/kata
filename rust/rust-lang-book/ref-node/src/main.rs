6778168dc76b94c56e6e84c808495625a2dcf1cb
SeHun Shin <s.shin@marathoner.kr>
2018-12-28 05:38:10 +0000

    ch15-29: 참조 카운트 시험하기

--- rust/rust-lang-book/ref-node/src/main.rs
+++ rust/rust-lang-book/ref-node/src/main.rs
@@ -15,17 +15,32 @@ fn main() {
         children: RefCell::new(vec![]),
     });
 
-    println!("leaf = {:?}", leaf);
+    visualize(&leaf, "leaf");
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!("branch = {:?}", branch);
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        visualize(&branch, "branch");
+        visualize(&leaf, "leaf");
+    }
+
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    visualize(&leaf, "leaf");
+}
 
-    let branch = Rc::new(Node {
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
-
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-
-    println!("leaf = {:?}", leaf);
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+fn visualize(node: &Rc<Node>, name: &str) {
+    println!(
+        "{} strong = {}, weak = {}",
+        name,
+        Rc::strong_count(&node),
+        Rc::weak_count(&node)
+    );
 }
